14214502132cc875ee6853e2a1df0fb922719e4d
UnrealKazu <9115757+UnrealKazu@users.noreply.github.com>
2021-12-05 15:56:26 +0100

    Add both puzzles for day 5

---
+++ day5/first/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+
+	"github.com/advent-of-code-2021/input"
+)
+
+const (
+	DiagramSize int = 1000
+)
+
+type Interval struct {
+	Start int
+	Stop  int
+}
+
+func main() {
+	in := input.Read("input.txt")
+
+	r := regexp.MustCompile(`([\d]+),([\d]+) -> ([\d]+),([\d]+)`)
+
+	diag := [DiagramSize][DiagramSize]int{}
+
+	overlaps := 0
+
+	for _, el := range in {
+		m := r.FindStringSubmatch(el)
+
+		x1, _ := strconv.Atoi(m[1])
+		y1, _ := strconv.Atoi(m[2])
+		x2, _ := strconv.Atoi(m[3])
+		y2, _ := strconv.Atoi(m[4])
+
+		if x1 == x2 {
+			// horizontal line
+			if y1 > y2 {
+				// swap the variables so that we have a consistent for-loop
+				y1, y2 = y2, y1
+			}
+
+			for i := y1; i <= y2; i++ {
+				diag[i][x1]++
+
+				if diag[i][x1] == 2 {
+					// we've got overlapping lines, but only count first overlap
+					overlaps++
+				}
+			}
+		} else if y1 == y2 {
+			// vertical line
+			if x1 > x2 {
+				x1, x2 = x2, x1
+			}
+
+			for i := x1; i <= x2; i++ {
+				diag[y1][i]++
+
+				if diag[y1][i] == 2 {
+					// we've got overlapping lines, but only count first overlap
+					overlaps++
+				}
+			}
+		}
+	}
+
+	fmt.Printf("Number of overlapping points %d\n", overlaps)
+}
---
+++ day5/second/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+
+	"github.com/advent-of-code-2021/input"
+)
+
+const (
+	DiagramSize int = 1000
+)
+
+type Interval struct {
+	Start int
+	Stop  int
+}
+
+func main() {
+	in := input.Read("input.txt")
+
+	r := regexp.MustCompile(`([\d]+),([\d]+) -> ([\d]+),([\d]+)`)
+
+	diag := [DiagramSize][DiagramSize]int{}
+
+	overlaps := 0
+
+	for _, el := range in {
+		m := r.FindStringSubmatch(el)
+
+		x1, _ := strconv.Atoi(m[1])
+		y1, _ := strconv.Atoi(m[2])
+		x2, _ := strconv.Atoi(m[3])
+		y2, _ := strconv.Atoi(m[4])
+
+		if x1 == x2 {
+			// horizontal line
+			if y1 > y2 {
+				// swap the variables so that we have a consistent for-loop
+				y1, y2 = y2, y1
+			}
+
+			for i := y1; i <= y2; i++ {
+				diag[i][x1]++
+
+				if diag[i][x1] == 2 {
+					// we've got overlapping lines, but only count first overlap
+					overlaps++
+				}
+			}
+		} else if y1 == y2 {
+			// vertical line
+			if x1 > x2 {
+				x1, x2 = x2, x1
+			}
+
+			for i := x1; i <= x2; i++ {
+				diag[y1][i]++
+
+				if diag[y1][i] == 2 {
+					// we've got overlapping lines, but only count first overlap
+					overlaps++
+				}
+			}
+		} else {
+			// diagonal line
+			var max int
+			var xDir int
+			var yDir int
+
+			// diagonal lines are always 45 degrees, so the num of steps in both
+			// directions is equal, so max can be calculated based on one dir
+			// also, determine the direction for the X axis
+			if x1 > x2 {
+				max = x1 - x2
+				xDir = -1
+			} else {
+				max = x2 - x1
+				xDir = 1
+			}
+
+			// determine the direction for the Y axis
+			if y1 > y2 {
+				yDir = -1
+			} else {
+				yDir = 1
+			}
+
+			for i := 0; i <= max; i++ {
+				diag[y1+(i*yDir)][x1+(i*xDir)]++
+
+				if diag[y1+(i*yDir)][x1+(i*xDir)] == 2 {
+					// we've got overlapping lines, but only count first overlap
+					overlaps++
+				}
+			}
+		}
+	}
+
+	fmt.Printf("Number of overlapping points %d\n", overlaps)
+}

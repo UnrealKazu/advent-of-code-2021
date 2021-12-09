255c9dad4ccb262cfd9676f97bffb416b18bd210
UnrealKazu <9115757+UnrealKazu@users.noreply.github.com>
2021-12-09 18:57:02 +0100

    Remove unnecessary neighbours tracking
    The neighbours slice is no longer necessary, because we track them in-place in the heatmap itself

--- day9/second/main.go
+++ day9/second/main.go
@@ -30,94 +30,68 @@ func main() {
 }
 
 func getBasinSize(p Point, hm *[][]int) int {
-	neighs := make(map[int]map[int]int)
+	count := 0
 	unchecked := []Point{}
 
-	addToMap(p.i, p.j, neighs)
 	(*hm)[p.i][p.j] = 10
+	count++
 
 	// fill the unchecked with a first round
-	getNonBoundaryNeighbours(p.i, p.j, hm, neighs, &unchecked)
+	getNonBoundaryNeighbours(p.i, p.j, hm, &unchecked, &count)
 
 	for len(unchecked) > 0 {
 		cur := unchecked[0]
 
-		getNonBoundaryNeighbours(cur.i, cur.j, hm, neighs, &unchecked)
+		getNonBoundaryNeighbours(cur.i, cur.j, hm, &unchecked, &count)
 
 		// remove the just checked point
 		unchecked = unchecked[1:]
 	}
 
 	// do a count on all neighbours
-	count := 0
-
-	for _, i := range neighs {
-		count += len(i)
-	}
-
 	return count
 }
 
-func getNonBoundaryNeighbours(i, j int, hm *[][]int, neighs map[int]map[int]int, unchecked *[]Point) {
+func getNonBoundaryNeighbours(i, j int, hm *[][]int, unchecked *[]Point, count *int) {
 	// left neigbour
 	if j > 0 && (*hm)[i][j-1] < 9 {
-		if addToMap(i, j-1, neighs) {
-			*unchecked = append(*unchecked, Point{
-				i: i,
-				j: j - 1,
-			})
-		}
+		*unchecked = append(*unchecked, Point{
+			i: i,
+			j: j - 1,
+		})
 		(*hm)[i][j-1] = 10
+		(*count)++
 	}
 
 	// top neighbour
 	if i > 0 && (*hm)[i-1][j] < 9 {
-		if addToMap(i-1, j, neighs) {
-			*unchecked = append(*unchecked, Point{
-				i: i - 1,
-				j: j,
-			})
-		}
+		*unchecked = append(*unchecked, Point{
+			i: i - 1,
+			j: j,
+		})
 		(*hm)[i-1][j] = 10
+		(*count)++
 	}
 
 	// right neigbour
 	if j < len((*hm)[i])-1 && (*hm)[i][j+1] < 9 {
-		if addToMap(i, j+1, neighs) {
-			*unchecked = append(*unchecked, Point{
-				i: i,
-				j: j + 1,
-			})
-		}
+		*unchecked = append(*unchecked, Point{
+			i: i,
+			j: j + 1,
+		})
 		(*hm)[i][j+1] = 10
+		(*count)++
 	}
 
 	// bottom neighbour
 	if i < len((*hm))-1 && (*hm)[i+1][j] < 9 {
-		if addToMap(i+1, j, neighs) {
-			*unchecked = append(*unchecked, Point{
-				i: i + 1,
-				j: j,
-			})
-		}
+		*unchecked = append(*unchecked, Point{
+			i: i + 1,
+			j: j,
+		})
 		(*hm)[i+1][j] = 10
+		(*count)++
 	}
-}
-
-func addToMap(i, j int, neighs map[int]map[int]int) bool {
-	if _, ok := neighs[i]; ok {
-		// i is in the map
-		if _, inOk := neighs[i][j]; inOk {
-			// j is also in the map, nothing to add
-			return false
-		}
-	} else {
-		// i doesn't exist yet, so make it so we can add it
-		neighs[i] = make(map[int]int)
-	}
-
-	neighs[i][j] = 1
-	return true
 }
 
 func getLowestPoints(hm [][]int) []Point {
